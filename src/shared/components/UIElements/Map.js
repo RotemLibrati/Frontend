65e99b35a18509d991fbad909b9fbaede7cddd76
RotemLibrati <rotemlibrati1@gmail.com>
2022-02-27 16:41:20 +0200

    Map component

---
+++ src/shared/components/UIElements/Map.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Map = props => {
+    return (
+        <div className={`map ${props.className}`} style={props.style }></div>
+    );
+
+};
+
+export default Map;
